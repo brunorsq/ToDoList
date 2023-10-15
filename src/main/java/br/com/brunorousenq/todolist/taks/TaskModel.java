4e64dc9968054170df870045a7d38111b20a6bb4
unknown <bruno.rsq@hotmail.com>
2023-10-15 14:30:20 -0300

    Criação da model de Tasks

--- src/main/java/br/com/brunorousenq/todolist/taks/TaskModel.java
+++ src/main/java/br/com/brunorousenq/todolist/taks/TaskModel.java
@@ -1,4 +1,4 @@
-package br.com.brunorousenq.todolist.user;
+package br.com.brunorousenq.todolist.taks;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -11,19 +11,24 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Data;
 
-@Data 
-@Entity(name="tb_users")
-public class UserModel {
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
 
     @Id
     @GeneratedValue(generator = "UUID")
     private UUID id;
+    private String description;
 
-    @Column(unique = true)
-    private String username;
-    private String name;
-    private String password;
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+    private UUID idUser;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
+
+    
 }
