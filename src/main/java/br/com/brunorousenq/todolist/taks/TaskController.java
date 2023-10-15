18f2d951198298dd7080f7990cf22af9c5b725e5
unknown <bruno.rsq@hotmail.com>
2023-10-15 15:19:52 -0300

    Buscar o usuário autenticado na criação de task

--- src/main/java/br/com/brunorousenq/todolist/taks/TaskController.java
+++ src/main/java/br/com/brunorousenq/todolist/taks/TaskController.java
@@ -1,10 +1,14 @@
 package br.com.brunorousenq.todolist.taks;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/tasks")
@@ -14,7 +18,9 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel){
+    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request){
+        var userId = request.getAttribute("userId");
+        taskModel.setIdUser((UUID) userId);
         var taskCreated = this.taskRepository.save(taskModel);
         return taskCreated;
     }
