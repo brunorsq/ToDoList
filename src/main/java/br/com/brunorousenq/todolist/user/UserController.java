8c7896707eee4a58b046661922f809f01bf9e39d
unknown <bruno.rsq@hotmail.com>
2023-10-15 13:45:30 -0300

    Criação da entidade de usuários no banco de dados e interface para manipulação

--- src/main/java/br/com/brunorousenq/todolist/user/UserController.java
+++ src/main/java/br/com/brunorousenq/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.brunorousenq.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void Create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getNome());
+    public UserModel Create(@RequestBody UserModel userModel) {
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
     }
     
 }
